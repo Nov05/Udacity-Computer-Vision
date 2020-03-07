88c47374c51a582dc79c30eec0b942a2f63a97ef
nov05 <arwen.liuwenjing@gmail.com>
2020-03-07 02:18:54 -0600

    2020-03-07

--- P2_Image_Captioning/model.py
+++ P2_Image_Captioning/model.py
@@ -25,7 +25,8 @@ class EncoderCNN(nn.Module):
 
 
 class DecoderRNN(nn.Module):
-    def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
+    def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1, 
+                 p_dropout=0.1):
         super().__init__()
 
         self.hidden_size = hidden_size
@@ -36,7 +37,7 @@ class DecoderRNN(nn.Module):
             embedding_dim=embed_size,
             # padding_idx=dictionary.pad(),
             )
-        self.dropout = nn.Dropout(p=0.1)
+        self.dropout = nn.Dropout(p=p_dropout)
 
         self.lstm = nn.LSTM(
             # For the first layer we'll concatenate the Encoder's final hidden
@@ -44,8 +45,8 @@ class DecoderRNN(nn.Module):
             input_size=embed_size,
             hidden_size=hidden_size,
             num_layers=num_layers,
-            )
-
+            batch_first=True)
+    
         self.linear = nn.Linear(hidden_size, vocab_size)
 
         # initialize the hidden state (see code below)
@@ -53,69 +54,60 @@ class DecoderRNN(nn.Module):
 
     def init_hidden(self):
         ''' At the start of training, we need to initialize a hidden state;
-           there will be none because the hidden state is formed based on perviously seen data.
-           So, this function defines a hidden state with all zeroes and of a specified size.'''
+            there will be none because the hidden state is formed based on perviously seen data.
+            So, this function defines a hidden state with all random numbers and of a specified size.'''
         # The axes dimensions are [n_layers, batch_size, hidden_size]
-        return (torch.zeros(1, 1, self.hidden_size),
-                torch.zeros(1, 1, self.hidden_size))
+        return (torch.randn(1, 1, self.hidden_size),
+                torch.randn(1, 1, self.hidden_size))
 
     def forward(self, features, captions):
-        # shape of features: torch.Size([10, 256])
-        # shape of captions: torch.Size([10, 13])
-        batch_size, sequence_size = captions.size() # [batch_size, sequence_size]
-
-        # LSTM input shape: [sequence_size, batch_size, input_size]
-        _, self.hidden = self.lstm(features.view(1, batch_size, -1))
+        # shape of features: [batch_size, embed_size], e.g. [10, 256]
+        # shape of captions: [batch_size, sequence_size], e.g. [10, 20]
        
         # Embed the target sequence, which has been shifted right by one
         # position and now starts with the end-of-sentence symbol.
-        # shape of caption_embedding: torch.Size([10, 13, 256])
-        caption_embedding = self.embedding(captions)
+        # shape of caption embedded: [10, 20, 256]
+        embedded = self.embedding(captions)
+        embedded = self.dropout(embedded)
+        lstm_input = torch.cat((features.unsqueeze(1), embedded), dim=1)
+        lstm_input = lstm_input[:, :-1, :] # remove the last token in the sequence
 
         # get the output and hidden state by passing the lstm over our word embeddings
         # the lstm takes in our embeddings and hiddent state
-        # shape of lstm_output: torch.Size([13, 10, 512])
-        lstm_output, self.hidden = self.lstm(
-            caption_embedding.view(sequence_size, batch_size, -1), 
-            self.hidden)
+        # LSTM input shape: [batch_size, sequence_size, input_size], e.g. [10, 20, 256]
+        # LSTM output shape: [batch_size, sequence_size, hidden_size], e.g. [10, 20, 512]
+        lstm_output, _ = self.lstm(lstm_input) # LSTM output, hidden state
 
-        # shape of outputs: torch.Size([13, 10, 9955])
-        outputs = self.linear(lstm_output)
-        outputs = outputs.transpose(1, 0)
-        outputs = F.log_softmax(outputs, dim=2)
-        return outputs
+        # shape of output: [batch_size, sequence_size, vocab_size], e.g.[10, 20, 9955]
+        output = self.linear(lstm_output)
+        output = F.log_softmax(output, dim=2)
+        
+        return output
 
     def sample(self, features, states=None, max_len=20):
-        " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
-        '''
-        Inference: There are multiple approaches that can be used
-        to generate a sentence given an image, with NIC. The first
-        one is Sampling where we just sample the first word according 
-        to p1, then provide the corresponding embedding
-        as input and sample p2, continuing like this until we sample
-        the special end-of-sentence token or some maximum length.
-        https://arxiv.org/pdf/1411.4555.pdf
-        '''
+        '''accepts pre-processed image tensor (features) and returns 
+           predicted sentence (list of tensor ids of length max_len)'''
+        
+        # Inference: There are multiple approaches that can be used
+        # to generate a sentence given an image, with NIC. The first
+        # one is Sampling where we just sample the first word according 
+        # to p1, then provide the corresponding embedding
+        # as input and sample p2, continuing like this until we sample
+        # the special end-of-sentence token or some maximum length.
+        # https://arxiv.org/pdf/1411.4555.pdf
+        
         # shape of features: torch.Size([1, 256])
-        batch_size, _ = features.size()
-        lstm_output, self.hidden = self.lstm(features.view(1, batch_size, -1))
-        outputs = self.linear(lstm_output)
-        outputs = outputs.transpose(1, 0)
-        outputs = F.log_softmax(outputs, dim=2)
-        _, idx = torch.max(outputs[0][0], 0)
         # shape of word_embedding [1, 1, 256]
-        word_embedding = self.embedding(idx.unsqueeze(0).unsqueeze(0)) 
-
+        lstm_input = features.unsqueeze(1)
+        
         idxs = []
         for _ in range(max_len):
-            lstm_output, self.hidden = self.lstm(
-                word_embedding.view(1, batch_size, -1), 
-                self.hidden)
-            outputs = self.linear(lstm_output)
-            outputs = outputs.transpose(1, 0)
-            outputs = F.log_softmax(outputs, dim=2)
-            _, idx = torch.max(outputs[0][0], 0)
+            lstm_output, states = self.lstm(lstm_input, states)
+            output = self.linear(lstm_output)
+            _, idx = torch.max(output[0][0], 0)
             idxs.append(idx.item())
-            word_embedding = self.embedding(idx.unsqueeze(0).unsqueeze(0))
+            # embedding input shape [batch_size, sequence_size]
+            # embedding output shape [batch_size, sequence_size, embed_size]
+            lstm_input = self.embedding(idx.unsqueeze(0).unsqueeze(0))
 
         return idxs
---
+++ files/notes.md
@@ -0,0 +1,7 @@
+
+
+
+### Automatic Image Captioning with CNN & RNN
+A Pytorch implementation of the CNN+RNN architecture on the MS-COCO dataset.  
+Krunal Kshirsagar, Jan 20  
+https://towardsdatascience.com/automatic-image-captioning-with-cnn-rnn-aae3cd442d83  
